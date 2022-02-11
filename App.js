56dc988aac4e2009993df08c6c438123edc0ebf4
rik <rikgeboers@gmail.com>
2022-02-11 13:38:55 +0100

    feat(restaurant component): restaurant card done
    styled card fully worktout

--- App.js
+++ App.js
@@ -1,12 +1,30 @@
 import React from "react";
 import { StatusBar as ExpoStatusBar } from "expo-status-bar";
 import { RestaurantsScreen } from "./src/feature/restaurants/screens/restaurants.screen";
+import { ThemeProvider } from "styled-components";
+import { theme } from "./src/infrastucture/theme";
+import {
+  useFonts as useOswald,
+  Oswald_400Regular,
+} from "@expo-google-fonts/oswald";
+import { useFonts as useLato, Lato_400Regular } from "@expo-google-fonts/lato";
 
 export default function App() {
+  const [oswaldLoaded] = useOswald({
+    Oswald_400Regular,
+  });
+  const [latoLoaded] = useLato({
+    Lato_400Regular,
+  });
+  if (!oswaldLoaded || !latoLoaded) {
+    return null;
+  }
   return (
     <>
-      <RestaurantsScreen />
-      <ExpoStatusBar style="auto" />
+      <ThemeProvider theme={theme}>
+        <RestaurantsScreen />
+        <ExpoStatusBar style="auto" />
+      </ThemeProvider>
     </>
   );
 }
--- src/feature/restaurants/screens/restaurants.screen.js
+++ src/feature/restaurants/screens/restaurants.screen.js
@@ -3,25 +3,21 @@ import { View, SafeAreaView } from "react-native";
 import { Searchbar } from "react-native-paper";
 import { getStatusBarHeight } from "react-native-status-bar-height";
 import styled from "styled-components";
-import { RestaurantInfoCard } from "../components/restaurant-info-card.component";
+import { RestaurantInfoCard } from "../components/restaurant-info-card";
 const SafeArea = styled(SafeAreaView)`
   flex: 1;
   margin-top: ${getStatusBarHeight()}px;
 `;
 const SearchContainer = styled(View)`
-  padding: 16px;
+  padding: ${(props) => props.theme.space[3]};
 `;
-const RestaurantListContainer = styled(View)`
-  flex: 1;
-  padding: 16px;
-`;
+
 export const RestaurantsScreen = () => (
   <SafeArea>
     <SearchContainer>
       <Searchbar />
     </SearchContainer>
-    <RestaurantListContainer>
-      <RestaurantInfoCard />
-    </RestaurantListContainer>
+
+    <RestaurantInfoCard />
   </SafeArea>
 );
